8a0a624d4cd59e66ee5d8cb19eb167efa0be3d5a
telamonian <telamonian@users.noreply.github.com>
2024-01-13 04:14:56 -0500

    added basic `ResyApi` with `get` and `set`. Untested

---
+++ resy_sniper/resyApi.py
@@ -0,0 +1,34 @@
+from requests import get, post
+
+class ResyApi:
+    def __init__(self, api_key, auth_token):
+        self.api_key = api_key
+        self.auth_token = auth_token
+
+        self.baseUrl = "https://api.resy.com"
+
+    @property
+    def headers(self):
+        return {
+            "Authorization": f'ResyAPI api_key="{self.api_key}"',
+            "x-resy-auth-token": self.auth_token,
+        }
+
+    def get(self, path, params):
+        url = self.baseUrl + path
+
+        return get(url, headers=self.headers, params=params)
+    
+    def post(self, path, params):
+        url = self.baseUrl + path
+
+        headers = {**self.headers, **{
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Origin": "https://widgets.resy.com",
+            "Referer": "https://widgets.resy.com/",
+        }}
+
+        # the post body accepts params stringified a-la query parameters, for some reason
+        body = "&".join(f"{k}={v}" for k,v in params.items())
+
+        return post(url, headers=headers, data=body)
